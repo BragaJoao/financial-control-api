5105c9fffe620b1932abec9eb00bd0c72f116cae
BragaJoao <joaogbraga@hotmail.com.br>
2022-08-13 08:50:55 -0300

    build: database

---
+++ src/index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const connectDatabase = require('./database/database')
+
+const port = process.env.PORT || 3001;
+const app = express();
+
+connectDatabase();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, () =>{
+    console.log(`Server running at http://localhost:${port}`)
+});
